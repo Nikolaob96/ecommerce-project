63be64e1d3efb9e1a4d00d6d324780903773f275
unknown <nikolaobradovicpfl@gmail.com>
2019-04-21 16:45:03 +0200

    Added project Lombok

--- ecommerce-backend/src/main/java/com/ecommnjt/model/User.java
+++ ecommerce-backend/src/main/java/com/ecommnjt/model/User.java
@@ -4,14 +4,25 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+   
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class User {
 	
 	@Id
 	@GeneratedValue
+	@Getter @Setter
 	private int id;
+	@Getter @Setter
 	private String username;
+	@Getter @Setter
 	private String password;
+	@Getter @Setter
 	private String email;
 	
 }
