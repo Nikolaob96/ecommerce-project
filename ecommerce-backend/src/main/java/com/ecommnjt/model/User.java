aacbf645c75f7604e8367a3ba7e1afacdd5e894f
unknown <nikolaobradovicpfl@gmail.com>
2019-04-21 16:06:25 +0200

    Started building package hierarchy and added User domain object

---
+++ ecommerce-backend/src/main/java/com/ecommnjt/model/User.java
@@ -0,0 +1,17 @@
+package com.ecommnjt.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	private String username;
+	private String password;
+	private String email;
+	
+}
