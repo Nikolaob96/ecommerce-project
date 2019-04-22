78dabf87ffddf6112c71a70fbf7b30458634b368
unknown <nikolaobradovicpfl@gmail.com>
2019-04-22 18:24:35 +0200

    Added service layer, changed db properties

--- ecommerce-backend/src/main/java/com/ecommnjt/controller/UserController.java
+++ ecommerce-backend/src/main/java/com/ecommnjt/controller/UserController.java
@@ -1,19 +1,33 @@
 package com.ecommnjt.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecommnjt.dto.UserDTO;
+import com.ecommnjt.model.User;
+import com.ecommnjt.service.UserService;
 
 @RestController
 @RequestMapping("/api")
 public class UserController {
+	
+	@Autowired
+	private UserService userService;
 
 	@PostMapping("/login")
 	public ResponseEntity<UserDTO> login() {
 		return new ResponseEntity<>(new UserDTO(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/getUser")
+	public ResponseEntity<UserDTO> testEndpoint() {
+		User user = userService.findById(1).get();
+		UserDTO userDTO = new UserDTO(user);
+		return new ResponseEntity<>(userDTO, HttpStatus.OK);
 	}
 }
---
+++ ecommerce-backend/src/main/java/com/ecommnjt/service/UserService.java
@@ -0,0 +1,20 @@
+package com.ecommnjt.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ecommnjt.model.User;
+import com.ecommnjt.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public Optional<User> findById(int id) {
+		return userRepository.findById(id);
+	}
+}
