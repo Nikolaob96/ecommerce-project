7c3299d41a11950fe7546d0f2d1cdf37fcb330fe
Nikola <nikolaobradovicpfl@gmail.com>
2019-05-13 19:05:00 +0200

    Created endpoint to retrieve all products from db

--- ecommerce-backend/src/main/java/com/ecommnjt/controller/ProductController.java
+++ ecommerce-backend/src/main/java/com/ecommnjt/controller/ProductController.java
@@ -1,13 +1,20 @@
 package com.ecommnjt.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecommnjt.dto.ProductDTO;
+import com.ecommnjt.model.Product;
 import com.ecommnjt.service.ProductService;
 
 @RestController
@@ -23,4 +30,18 @@ public class ProductController {
 	public void addProducts(@RequestBody ProductDTO productDTO) {
 		productService.addProduct(ProductDTO.getProduct(productDTO));
 	}
+	
+	@GetMapping("/getProducts")
+	@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+	public ResponseEntity<List<ProductDTO>> getProducts() {
+		List<Product> products = productService.getProducts();
+		List<ProductDTO> productsDTO = new ArrayList<>();
+		
+		for (Product product : products) {
+			productsDTO.add(new ProductDTO(product));
+		}
+		
+		return new ResponseEntity<>(productsDTO,HttpStatus.OK);
+	}
+	
 }
--- ecommerce-backend/src/main/java/com/ecommnjt/service/ProductService.java
+++ ecommerce-backend/src/main/java/com/ecommnjt/service/ProductService.java
@@ -1,5 +1,7 @@
 package com.ecommnjt.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,5 +16,9 @@ public class ProductService {
 	
 	public void addProduct(Product product) {
 		productRepository.save(product);
+	}
+	
+	public List<Product> getProducts() {
+		return productRepository.findAll();
 	}
 }
